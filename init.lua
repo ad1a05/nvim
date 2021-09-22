d7206282c083a09e29ec56d37de38ec2509f1616
Weike Yu <weike.yu.c@gmail.com>
2021-09-23 01:47:20 +0800

    init

---
+++ init.lua
@@ -0,0 +1,146 @@
+
+vim.g.mapleader = " "
+
+require('plugins')
+
+require('nightfox').load()
+
+require('lualine').setup {
+  options = {
+    theme = "nightfox"
+  }
+}
+
+require('telescope').setup {}
+require('telescope').load_extension('fzf')
+
+-- lsp config
+local nvim_lsp = require('lspconfig')
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'pyright', 'rust_analyzer', 'clangd' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+end
+
+-- completion config
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+-- luasnip setup
+local luasnip = require 'luasnip'
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if vim.fn.pumvisible() == 1 then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+      elseif luasnip.expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if vim.fn.pumvisible() == 1 then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
+      elseif luasnip.jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
+
+-- Some basic settings
+vim.cmd('set nu')
+vim.cmd('set expandtab') -- replace tab with space
+vim.cmd('set shiftwidth=4')
+vim.cmd('set tabstop=4')
+vim.cmd('set softtabstop=4')
+vim.cmd('set smarttab')
+vim.cmd('set backspace=eol,start,indent')
+vim.cmd('set showmatch')
+vim.cmd('set mat=1')
+vim.cmd('set so=7')
+vim.cmd('set wildmenu')
+vim.cmd('set ruler')
+vim.cmd('set showcmd')
+vim.cmd('set hidden')
+
+vim.api.nvim_set_keymap('i', '<C-a>', '<Home>', { noremap=true })
+vim.api.nvim_set_keymap('i', '<C-e>', '<End>', { noremap=true })
+vim.api.nvim_set_keymap('i', '<C-f>', '<right>', { noremap=true })
+vim.api.nvim_set_keymap('i', '<C-b>', '<left>', { noremap=true })
+vim.api.nvim_set_keymap('i', '<C-p>', '<up>', { noremap=true })
+vim.api.nvim_set_keymap('i', '<C-n>', '<down>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>bd', '<cmd>bd<cr>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>bp', '<cmd>bp<cr>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>bn', '<cmd>bn<cr>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>bl', '<cmd>b#<cr>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>cp', '<cmd>cp<cr>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>cn', '<cmd>cn<cr>', { noremap=true })
+
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap=true })
+
---
+++ lua/plugins.lua
@@ -0,0 +1,41 @@
+-- To get started, first clone this repository to somewhere on your packpath, e.g.:
+-- git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+--  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- colorscheme
+  use 'EdenEast/nightfox.nvim'
+
+  -- lualine
+  use {
+    'hoob3rt/lualine.nvim', requires = {
+      'kyazdani42/nvim-web-devicons',
+      opt = true
+    }
+  }
+
+  -- telescope
+  use {
+    'nvim-telescope/telescope.nvim', requires = {
+      'nvim-lua/plenary.nvim'
+    }
+  }
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'make'
+  }
+
+  -- lsp
+  use 'neovim/nvim-lspconfig'
+
+  -- completion
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+
+end)
+
